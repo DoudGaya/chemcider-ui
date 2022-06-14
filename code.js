8ce290045fe8fe472265560c7a095c67d2d99ef5
Dauda Abdulrahman Gaya <arbdoule@yahoo.com>
2022-06-14 11:36:39 +0100

    tailwind setup

---
+++ code.js
@@ -0,0 +1,14 @@
+let greet1 = 'Hello'
+const greet2 = ' HI'
+
+let message1 = 'World'
+let message2 = ' Everyone'
+
+
+greet1 = `${greet1} ${message1}`
+// greet2 = `${greet2} ${message2}`
+
+
+console.log(greet1)
+console.log(greet2)
+
