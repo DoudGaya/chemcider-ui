5dff187c17f7e4c2281a436ac128469ef740d60e
Dauda Abdulrahman Gaya <arbdoule@yahoo.com>
2022-06-18 22:41:21 +0100

    a

--- code.js
+++ code.js
@@ -1,14 +1,11 @@
-let greet1 = 'Hello'
-const greet2 = ' HI'
+class Sample {
+    constructor(name, type, date) {
+        this.name = name,
+        this.type = type,
+        this.date = date
+    }
 
-let message1 = 'World'
-let message2 = ' Everyone'
-
-
-greet1 = `${greet1} ${message1}`
-// greet2 = `${greet2} ${message2}`
-
-
-console.log(greet1)
-console.log(greet2)
-
+    call() {
+        return 
+    }
+}
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
-<div class=" py-[13px]">
-    <div class=" flex items-center space-y-2 flex-col  py-32 mt-28">
+<div class="pb-[1px]">
+    <div class=" flex items-center space-y-2 flex-col  md:py-24 mt-28">
         <h2 class=" font-logo md:text-4xl text-2xl tracking-wider font-thin" @mouseover="changeLogo">{{ logoText }}</h2>
         <h3 class="  md:text-xl tracking-wider font-thin">{{ slogan }}</h3>
         <hr class=" w-32 border hidden md:block">
@@ -65,8 +65,10 @@
 
     </div>
 
-    <div class="md:fixed hidden bottom-16 right-16">
-        hello
+    <div class="md:block fixed hidden bg-gray-400/50 hover:bg-gray-400/25 p-3 rounded-md bottom-16 right-16">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+        </svg>
     </div>
 
 </div>
