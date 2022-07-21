0748fbc8518fbe4b29d4470efa9c994a094d5343
DoudGaya <arbdoule@yahoo.com>
2022-07-21 23:07:22 +0100

    links

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
 
 
 
@@ -15,6 +16,12 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: Login
+    },
+
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
     },
 ]
 
